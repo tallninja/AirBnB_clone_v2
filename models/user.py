f4cd006be3358ec8e33ff180a9133a8661b1b033
Ernest Wambua <ernestwambua2@gmail.com>
2022-09-13 08:40:26 +0300

    Update User model

--- models/user.py
+++ models/user.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
+    """This is the class for user"""
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
