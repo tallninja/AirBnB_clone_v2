fd3d8ec4a103fcd6378513d0b812ccdfe7bf8352
Ernest Wambua <ernestwambua2@gmail.com>
2022-09-29 22:00:13 +0300

    1-pack_web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,8 @@ def do_pack():
     """packages all contents from web_static into .tgz archive"""
     now = datetime.now().strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
-    output = local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
+    output = local("tar -czvf versions/web_static_{}.tgz web_static"
+                   .format(now))
     if output.failed:
         return None
     return output
