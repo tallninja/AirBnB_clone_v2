e5c2e14e18e7ffd7941a9921f7e0be38470a60f9
Ernest Wambua <ernestwambua2@gmail.com>
2022-09-29 18:49:40 +0000

    Bug Fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,7 @@ def do_pack():
     """packages all contents from web_static into .tgz archive"""
     now = datetime.now().strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
-    output = local(f"tar -czvf versions/web_static_{now}.tgz web_static")
+    output = local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
     if output.failed:
         return None
     return output
