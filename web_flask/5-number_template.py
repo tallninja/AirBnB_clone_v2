cd6d914637dd1ebeee16d8dea644ea3dd9a90ad8
Ernest Wambua <ernestwambua2@gmail.com>
2022-10-14 05:28:47 +0300

    5-number_template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def url_param(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    if type(n) == int:
+        return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    if type(n) == int:
+        return render_template('5-number.html', n=n)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
