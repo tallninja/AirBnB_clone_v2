62b85de622e0fab0cbe171f5a930d442d22d30d2
Ernest Wambua <ernestwambua2@gmail.com>
2022-10-16 07:05:23 +0300

    7-states_list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script to start a flask app on localhost"""
+from models import storage
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def appcontext_teardown(exc=None):
+    """called on teardown of app contexts"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def conditional_templating(n=None):
+    """checking input data using templating"""
+    return render_template('7-states_list.html',
+                           states=storage.all("State"))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
