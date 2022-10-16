7757af1dd807bd793395bc50831ca9bc82469c95
Ernest Wambua <ernestwambua2@gmail.com>
2022-10-16 06:29:10 +0300

    strict slashes

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """hello hbnb"""
     return "Hello HBNB!"
